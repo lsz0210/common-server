54738beb5b4e2855b5c741f2cf2d4722947dc49c
lishizhao <lishizhao@haier.com>
2021-02-09 16:58:23 +0800

    补充遗漏注解

--- src/main/java/com/duck/common/server/mapper/UserIdentyMapper.java
+++ src/main/java/com/duck/common/server/mapper/UserIdentyMapper.java
@@ -2,6 +2,7 @@ package com.duck.common.server.mapper;
 
 import com.duck.common.server.model.UserIdenty;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author generator@Lsz
  * @since 2021-02-09
  */
+@Mapper
 public interface UserIdentyMapper extends BaseMapper<UserIdenty> {
 
 }
